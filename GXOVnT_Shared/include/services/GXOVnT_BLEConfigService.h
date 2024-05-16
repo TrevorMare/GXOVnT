a261652b8a95c10f4bdffee2f4dc27f878192a7e
Trevor Mare <tmare@agr-us.com>
2024-05-16 12:22:47 -0500

    Updated dependencies

---
+++ GXOVnT_Shared/include/services/GXOVnT_BLEConfigService.h
@@ -0,0 +1,13 @@
+/////////////////////////////////////////////////////////////////
+#pragma once
+#ifndef _GXOVNT_BLECONFIGSERVICE_H
+#define _GXOVNT_BLECONFIGSERVICE_H
+
+// https://github.com/mhaberler/nanopb-test/blob/master/platformio.ini
+// https://medium.com/@mehmettopuz127/using-protocol-buffers-between-esp32-and-qt-qml-efficient-data-communication-a4bd7582d8b9
+// https://github.com/MarcGeh/esp32-cpp-protobuf
+// https://github.com/avinabmalla/ESP32_BleSerial/blob/master/examples/BleSerial_Bridge/BleSerial_Bridge.ino
+
+
+
+#endif
