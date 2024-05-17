85ecefa0d3ef7d9c4b988c44ae3849bd1e90b54e
Trevor Mare <tmare@agr-us.com>
2024-05-17 13:05:39 -0500

    Generate proto messages and copy to shared project

--- GXOVnT_ProtoMessage/scripts/generate-proto.ps1
+++ GXOVnT_ProtoMessage/scripts/generate-proto.ps1
@@ -1,11 +1,16 @@
 
+param (
+    
+    [string]$rootDirectory
+ )
+
 # Nanopd is used to generate the c++ file proto source files
 [string]$global:nanopd_git_url = "https://github.com/nanopb/nanopb/archive/refs/heads/master.zip"
 # Standard protoc is used to generate the c# proto source files
 [string]$global:protoc_git_url = "https://github.com/protocolbuffers/protobuf/releases/download/v26.1/protoc-26.1-win64.zip"
 
 # Input directories
-[string]$global:root_directory = "C:\src\Trevor\GXOVnT"
+[string]$global:root_directory = $rootDirectory.TrimStart().TrimEnd()
 [string]$global:root_input_directory = ""
 [string]$global:messages_input_directory = ""
 
@@ -25,9 +30,9 @@
 # Final Copy directories
 [string]$global:final_output_cpp_headers = ""
 [string]$global:final_output_cpp_source = ""
-
-
 function SetupInputAndOutputDirectories() {
+    Write-Host "[Powershell] Setting up input and output directory paths"
+
     $global:root_input_directory = "$global:root_directory\GXOVnT_ProtoMessage"
     $global:root_generator_directory = "$global:root_input_directory\generators"
     $global:root_generator_protoc_directory = "$global:root_generator_directory\protoc"
@@ -47,6 +52,7 @@ function SetupInputAndOutputDirectories() {
 }
 
 function PrepareGeneratorsDirectory() {
+    Write-Host "[Powershell] Creating input and output directories"
     #=============== Custom Proto Generators directories ===============
     if (!(Test-Path -Path $global:root_generator_directory)) {
         New-Item -ItemType Directory -Force -Path $global:root_generator_directory
@@ -85,7 +91,7 @@ function PrepareGeneratorsDirectory() {
 
 function DownloadAndExtract_protoc() {
     If ((Get-ChildItem -Path $global:root_generator_protoc_directory -Force | Measure-Object).Count -eq 0) {
-        
+        Write-Host "[Powershell] Downloading protoc libraries"
         $zipFile = "$global:root_generator_protoc_directory\protoc.zip"
         Invoke-WebRequest $global:protoc_git_url -OutFile $zipFile
         Expand-Archive $zipFile -DestinationPath $global:root_generator_protoc_directory
@@ -94,7 +100,7 @@ function DownloadAndExtract_protoc() {
 
 function DownloadAndExtract_nanopd() {
     If ((Get-ChildItem -Path $global:root_generator_nanopd_directory -Force | Measure-Object).Count -eq 0) {
-        
+        Write-Host "[Powershell] Downloading nanopd libraries"
         $zipFile = "$global:root_generator_nanopd_directory\nanopd.zip"
         Invoke-WebRequest $global:nanopd_git_url -OutFile $zipFile
         Expand-Archive $zipFile -DestinationPath $global:root_generator_nanopd_directory
@@ -102,11 +108,12 @@ function DownloadAndExtract_nanopd() {
 }
 
 function PrepareEnvironment() {
+    Write-Host "[Powershell] Installing protobuf and grpcio tools"
     pip install protobuf grpcio-tools
 }
 
 function GenerateFiles_CSharp() {
-
+    Write-Host "[Powershell] Generating C# files to " $global:src_output_csharp
     Get-ChildItem $global:messages_input_directory -Filter *.proto | Foreach-Object {
         $command = "$global:protoc_generator --proto_path=$global:messages_input_directory --csharp_out=$global:src_output_csharp $_"
         Invoke-Expression $command
@@ -114,6 +121,7 @@ function GenerateFiles_CSharp() {
 }
 
 function GenerateFiles_CPP() {
+    Write-Host "[Powershell] Generating C++ files to " $global:src_output_cpp
     Get-ChildItem $global:messages_input_directory -Filter *.proto | Foreach-Object {
         $command = "$global:protoc_generator --plugin=protoc-gen-nanopb=$global:nanopd_generator --proto_path=$global:messages_input_directory --nanopb_out=$global:src_output_cpp $_"
         Invoke-Expression $command
@@ -124,22 +132,29 @@ function CopyCPPFilesToShared() {
 
     Get-ChildItem $global:src_output_cpp -Filter *.h | Foreach-Object {
         
-        $destinationFileName = $global:final_output_cpp_headers + "\" + $_.NameString
-        Copy-Item $_ -Destination $destinationFileName -force 
+        $sourceFileName = $_.FullName
+        $destinationFileName = $global:final_output_cpp_headers + "\" + $_.Name
+        Copy-Item $sourceFileName -Destination $destinationFileName -force 
         (Get-Content $destinationFileName) -replace '<pb.h>', """pb.h""" | Set-Content $destinationFileName
     }
 
     Get-ChildItem $global:src_output_cpp -Filter *.c | Foreach-Object {
+        Write-Host "[Powershell] Copying c++ source files"
 
-        $oldIncludePath = """" + $_.NameString.Replace(".c", ".h") + """"
-        $newIncludePath = """messages/" + $_.NameString.Replace(".c", ".h") + """"
-        $destinationFileName = $global:final_output_cpp_source + "\" + $_.NameString
+        $sourceFileName = $_.FullName
+        $destinationFileName = $global:final_output_cpp_headers + "\" + $_.Name
+
+        $oldIncludePath = """" + $_.Name.Replace(".c", ".h") + """"
+        $newIncludePath = """messages/" + $_.Name.Replace(".c", ".h") + """"
+        $destinationFileName = $global:final_output_cpp_source + "\" + $_.Name
         # Copy the c file
-        Copy-Item $_ -Destination $destinationFileName -force 
+        Copy-Item $sourceFileName -Destination $destinationFileName -force 
         # fix the content
         (Get-Content $destinationFileName) -replace $oldIncludePath, $newIncludePath | Set-Content $destinationFileName
     }
-    
+
+    Write-Host "[Powershell] Copying c++ additional library files"
+
     Copy-Item "$global:nanopd_extra\pb.h" -Destination "$global:final_output_cpp_headers\pb.h" -force 
     Copy-Item "$global:nanopd_extra\pb_common.c" -Destination "$global:final_output_cpp_headers\pb_common.c" -force 
     Copy-Item "$global:nanopd_extra\pb_common.h" -Destination "$global:final_output_cpp_headers\pb_common.h" -force 
@@ -149,6 +164,8 @@ function CopyCPPFilesToShared() {
     Copy-Item "$global:nanopd_extra\pb_encode.c" -Destination "$global:final_output_cpp_headers\pb_encode.c" -force 
 
 }
+
+Write-Host "[Powershell] Starting generation of proto files: " $rootDirectory
 
 SetupInputAndOutputDirectories
 PrepareGeneratorsDirectory
--- GXOVnT_Shared/scripts/build-proto-messages.py
+++ GXOVnT_Shared/scripts/build-proto-messages.py
@@ -1,71 +1,10 @@
 Import("env")
 import os
-import shutil
-import hashlib
-import pathlib
-import shlex
-import subprocess
+import subprocess, sys
 
-import SCons.Action
-from platformio import fs
+# Get the root project directory, it should in theory always be one level up from the project directory
+rootDirectory = os.path.dirname(env.subst("$PROJECT_DIR"))
+scriptPath = os.path.join(rootDirectory, "GXOVnT_ProtoMessage", "scripts", "generate-proto.ps1")
 
-python_exe = env.subst("$PYTHONEXE")
-try:
-    import protobuf
-except ImportError:
-    print("[nanopb] Installing Protocol Buffers dependencies");
-
-    # We need to specify protobuf version. In other case got next (on Ubuntu 20.04):
-    # Requirement already satisfied: protobuf in /usr/lib/python3/dist-packages (3.6.1)
-    subprocess.run([python_exe, '-m', 'pip', 'install', "protobuf>=3.19.1"])
-
-try:
-    import grpc_tools.protoc
-except ImportError:
-    print("[nanopb] Installing gRPC dependencies");
-    subprocess.run([python_exe, '-m', 'pip', 'install', "grpcio-tools>=1.43.0"])
-
-print("---------------------------------")
-print("Starting build of proto messages")
-print("---------------------------------")
-
-# Get the root project directory, it should in theory always be one leve up from the project directory
-rootProject_dir = os.path.dirname(env.subst("$PROJECT_DIR"))
-# Now get the library directory and the message prototype directories
-messageProtoTypeDirSource = os.path.join(rootProject_dir, "GXOVnT_ProtoMessage", "proto")
-messageProtoTypeDirDest = os.path.join(rootProject_dir, "GXOVnT_Shared", "include", "messages", "generate")
-srcDirDest = os.path.join(rootProject_dir, "GXOVnT_Shared", "include", "messages")
-nanopb_generator = os.path.join(rootProject_dir, "GXOVnT_Shared", "scripts", "nanopb_generator.py")
-
-# If the source directory does not exist, quit the script
-if not os.path.exists(messageProtoTypeDirSource):
-    print("Proto message source does not exist at: " + messageProtoTypeDirSource)
-    exit()
-
-# Create the destination directory if it does not exist in the library location
-if not os.path.exists(messageProtoTypeDirDest):
-    os.mkdir(messageProtoTypeDirDest)
-
-# Clear out the files in the destination library directory
-for root, dirs, files in os.walk(messageProtoTypeDirDest):
-    for f in files:
-        os.unlink(os.path.join(root, f))
-    for d in dirs:
-        shutil.rmtree(os.path.join(root, d))
-
-# Copy the proto and option files from the source to the destination
-for root, dirs, files in os.walk(messageProtoTypeDirSource):
-    for f in files:
-        shutil.copy(os.path.join(root, f), os.path.join(messageProtoTypeDirDest, f))
-
-
-
-nanopb_options = []
-nanopb_options.extend(["--output-dir", srcDirDest])
-
-for root, dirs, files in os.walk(messageProtoTypeDirSource):
-    for f in files:
-        proto_file_basename = os.path.basename(f)
-        cmd = [python_exe, nanopb_generator] + nanopb_options + [proto_file_basename]
-        action = SCons.Action.CommandAction(cmd)
-        result = env.Execute(action)
+p = subprocess.Popen(["powershell.exe", scriptPath + " -rootDirectory " + rootDirectory ], stdout=sys.stdout)
+p.communicate
